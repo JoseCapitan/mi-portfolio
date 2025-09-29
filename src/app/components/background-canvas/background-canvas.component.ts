fced1591a0725267c16c0a5e612405ad08b36830
JoseCapitan <ercapitan7@gmail.com>
2025-09-29 12:37:16 +0200

    Initial config

---
+++ src/app/components/background-canvas/background-canvas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-background-canvas',
+  imports: [],
+  templateUrl: './background-canvas.component.html',
+  styleUrl: './background-canvas.component.scss'
+})
+export class BackgroundCanvasComponent {
+
+}
