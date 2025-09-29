8621aa14719c73b43a1f6bcadf0b5aaa99ab4235
JoseCapitan <ercapitan7@gmail.com>
2025-09-29 13:36:32 +0200

    configurando proyecto para levantarlo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HeroComponent } from './components/hero/hero.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { TimelineComponent } from './components/timeline/timeline.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HeroComponent },
+  { path: 'proyectos', component: ProjectsComponent },
+  { path: 'curriculum', component: TimelineComponent }, // o crea una vista específica
+  { path: 'contacto', component: ContactComponent },
+  { path: '**', redirectTo: '' } // Redirige rutas no encontradas al home
+];
